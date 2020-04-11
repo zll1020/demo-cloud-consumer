afce14dec3bdabb2d262326f4985aeac1e505d7c
zhangll6 <zhangll6@ziroom.com>
2020-04-11 12:41:22 +0800

    eureka

--- src/test/java/com/example/cloud/consumer/democloudconsumer/DemoCloudConsumerApplicationTests.java
+++ src/test/java/com/example/cloud/consumer/democloudconsumer/DemoCloudConsumerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.cloud.consumer.democloudconsumer;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
